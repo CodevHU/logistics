2407715fa0c04eafd8afea4e0d1a41f65dc88032
Attila Domokos <attila@codev.hu>
2022-12-09 17:43:09 +0100

    Add delay time to plannedTime

--- logistics/src/main/java/hu/codev/logistics/repository/SectionRepository.java
+++ logistics/src/main/java/hu/codev/logistics/repository/SectionRepository.java
@@ -1,5 +1,7 @@
 package hu.codev.logistics.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
@@ -11,4 +13,9 @@ import hu.codev.logistics.model.TransportPlan;
 public interface SectionRepository extends JpaRepository<Section, Long>, JpaSpecificationExecutor<Address> {
 	
 	Long countByTransportPlanAndFromMilestoneOrToMilestone(TransportPlan transportPlan, Milestone fromMilestone, Milestone toMilestone);
+
+	Section findByTransportPlanAndFromMilestoneOrToMilestone(TransportPlan transportPlan,
+			Milestone milestone, Milestone milestone2);
+
+	Optional<Section> findByTransportPlanAndNumber(TransportPlan transportPlan, long l);
 }
--- logistics/src/main/java/hu/codev/logistics/service/TransportPlanService.java
+++ logistics/src/main/java/hu/codev/logistics/service/TransportPlanService.java
@@ -1,5 +1,7 @@
 package hu.codev.logistics.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -7,6 +9,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 import hu.codev.logistics.dto.DelayDTO;
 import hu.codev.logistics.model.Milestone;
+import hu.codev.logistics.model.Section;
 import hu.codev.logistics.model.TransportPlan;
 import hu.codev.logistics.repository.MilestoneRepository;
 import hu.codev.logistics.repository.SectionRepository;
@@ -33,6 +36,25 @@ public class TransportPlanService {
 		
 		if(sectionRepository.countByTransportPlanAndFromMilestoneOrToMilestone(transportPlan,milestone,milestone) == 0)
 			throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+		
+		Section section = sectionRepository.findByTransportPlanAndFromMilestoneOrToMilestone(transportPlan,milestone,milestone);
+		milestone.setPlannedTime(milestone.getPlannedTime().plusMinutes(delay.getDelay()));
+		
+		if(section.getNumber() == 0) {
+			Milestone endMilestone = milestoneRepository.findById(section.getToMilestone().getId()).get();
+			endMilestone.setPlannedTime(endMilestone.getPlannedTime().plusMinutes(delay.getDelay()));
+		}
+		
+		if(delay.getMilestoneId() == section.getToMilestone().getId()) {
+			Optional<Section> nextSection = sectionRepository.findByTransportPlanAndNumber(transportPlan,section.getNumber() + 1);
+			
+			if(nextSection.isPresent()) {
+				Milestone nextFromMilestone = milestoneRepository.findById(nextSection.get().getFromMilestone().getId()).get();
+				nextFromMilestone.setPlannedTime(nextFromMilestone.getPlannedTime().plusMinutes(delay.getDelay()));
+				
+			}
+		}
+		
 		
 		
 		
